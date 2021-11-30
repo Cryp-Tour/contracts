998000efb081ddca9c40da9b78de5a2d9df2248b
Benedict Wetzel <benedict.f.wetzel@gmx.de>
2021-11-30 11:43:46 +0100

    Inherit from OpenZeppelin ERC20

--- contracts/TourToken.sol
+++ contracts/TourToken.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract TourToken {
-  constructor() public {
-  }
+contract TourToken is ERC20 {
+  constructor(string memory name, string memory symbol) ERC20(name, symbol)  {
+    }
 }

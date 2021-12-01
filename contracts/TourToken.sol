d719a9fbec92cceffdf7353c60202ba5e577086d
Benedict Wetzel <benedict.f.wetzel@gmx.de>
2021-12-01 14:09:25 +0100

    Add fixed Minting at start

--- contracts/TourToken.sol
+++ contracts/TourToken.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TourToken is ERC20 {
-  constructor(string memory name, string memory symbol) ERC20(name, symbol)  {
-    }
+  constructor(string memory name, string memory symbol, uint256 toMint) ERC20(name, symbol)  {
+    _mint(msg.sender, toMint);
+  }
 }

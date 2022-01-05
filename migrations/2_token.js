a17fe1cf78a4caee953fbe11e03abf3da0ef1809
wetzelbe <benedict.f.wetzel@gmx.de>
2022-01-05 14:21:42 +0100

    Remove old Contracts, that are replaced by Balancer and Ocean Contracts

--- migrations/2_token.js
+++
@@ -1,5 +0,0 @@
-const TourToken = artifacts.require("TourToken");
-
-module.exports = function(deployer) {
-  deployer.deploy(TourToken, "Cryp-Tour", "TOUR", 400);
-};
--- migrations/3_pool.js
+++
@@ -1,6 +0,0 @@
-const TourToken = artifacts.require("TourToken");
-const Pool = artifacts.require("Pool");
-
-module.exports = function(deployer) {
-  deployer.deploy(Pool, TourToken.address, "Pool", "POOL", 1);
-};

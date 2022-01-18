e3aa439025ab590dab1107242ac89994bfdbccf9
wetzelbe <benedict.f.wetzel@gmx.de>
2022-01-18 23:11:49 +0100

    Change naming

--- migrations/1641387386_factory_and_template.js
+++ migrations/1641387386_factory_and_template.js
@@ -1,9 +1,9 @@
-const TourTokenTemplate = artifacts.require("TourTokenTemplate");
-const TTFactory = artifacts.require("DTFactory");
+const TTTemplate = artifacts.require("TourTokenTemplate");
+const TTFactory = artifacts.require("TourTokenFactory");
 
 module.exports = function (_deployer, network, accounts) {
   // Deploy Template for all TourTokens
-  _deployer.deploy(TourTokenTemplate,
+  _deployer.deploy(TTTemplate,
     "TourToken Template",
     "TOURTEMP",
     accounts[0],
@@ -11,7 +11,7 @@ module.exports = function (_deployer, network, accounts) {
     "Just a Template",
     accounts[0]).then(() => {
       // Deploy TourToken-Factory
-      return _deployer.deploy(TTFactory, TourTokenTemplate.address,
+      return _deployer.deploy(TTFactory, TTTemplate.address,
         accounts[0])
     })
 };

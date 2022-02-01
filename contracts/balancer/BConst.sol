9f85921187c1453e2c1577338e261af962de7a77
wetzelbe <benedict.f.wetzel@gmx.de>
2022-02-01 18:59:11 +0100

    Add SPDX Identifiers

--- contracts/balancer/BConst.sol
+++ contracts/balancer/BConst.sol
@@ -8,6 +8,8 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
+// SPDX-License-Identifier: GNU
+
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 

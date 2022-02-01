7f0b8b248e48e06218ae5efbeade889fc964c0c1
wetzelbe <benedict.f.wetzel@gmx.de>
2022-02-01 19:33:13 +0100

    Add more tests

--- test/factory_test.js
+++ test/factory_test.js
@@ -1,18 +1,33 @@
 const TTFactory = artifacts.require("TourTokenFactory");
-
+const TourToken = artifacts.require("TourTokenTemplate");
 /*
  * uncomment accounts to access the test accounts made available by the
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
 contract("TTFactory", function (/* accounts */) {
-  it("should add one to Count", async function () {
+  it("is deployed", async function () {
+    factory = await TTFactory.deployed();
+    assert.notEqual(factory, undefined)
+  })
+  it("adds one to Count when creating a Token", async function () {
     factory = await TTFactory.deployed();
     let countbefore = await factory.getCurrentTokenCount.call();
-    console.log(countbefore)
     await factory.createToken("1234", "TOUR1234", "TOUR1234", 10)
     let countafter = await factory.getCurrentTokenCount.call();
-    console.log(countafter)
     return assert.equal(countbefore.toNumber() + 1, countafter);
+  });
+  it("Returns right number of Events when creating new TourToken", async function () {
+    factory = await TTFactory.deployed();
+    let newtoken = await factory.createToken("1234", "TOUR1234", "TOUR1234", 10)
+    return assert.equal(newtoken.logs.length, 3);
+  });
+  it("Returns right name after creating new TourToken", async function () {
+    factory = await TTFactory.deployed();
+    let creationEvent = await factory.createToken("1234", "TOUR1234", "TOUR1234", 10)
+    let newTourTokenAddress = creationEvent.logs[0].args['0'];
+    let newTourToken = await TourToken.at(newTourTokenAddress);
+    let name = await newTourToken.name.call();
+    return assert.equal(name, "TOUR1234");
   });
 });

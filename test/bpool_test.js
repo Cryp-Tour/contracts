4c49c56275045d7f028839d8043ccde1cbc4620d
wetzelbe <benedict.f.wetzel@gmx.de>
2022-02-01 22:04:03 +0100

    Update Tests

--- test/bpool_test.js
+++ test/bpool_test.js
@@ -1,16 +1,24 @@
-const bpool = artifacts.require("bpool");
-
+const BFactory = artifacts.require("BFactory");
+const BPool = artifacts.require("BPool");
 /*
  * uncomment accounts to access the test accounts made available by the
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("bpool", function (/* accounts */) {
+contract("BFactory", function (/* accounts */) {
   it("is deployed", async function () {
     let contr = undefined;
     try {
-      contr = await bpool.deployed();
+      contr = await BFactory.deployed();
     } catch (error) {}
     return assert.notEqual(contr, undefined);
+  });
+  it("creates new pools", async function () {
+    let factory = await BFactory.deployed();
+    let tx = await factory.newBPool();
+    let poolAddress = tx.logs[0].args.pool; 
+    let pool = await BPool.at(poolAddress);
+    assert.equal(await pool.symbol.call(), "CPT");
+
   });
 });

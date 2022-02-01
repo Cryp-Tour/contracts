8dc57461b40dd3ee22ebfc1ad906d1b0eb772bbb
wetzelbe <benedict.f.wetzel@gmx.de>
2022-02-01 20:59:07 +0100

    Organize Migrations

---
+++ migrations/3_balancer_factory.js
@@ -0,0 +1,7 @@
+const BPool = artifacts.require("BPool");
+const BFactory = artifacts.require("BFactory");
+
+
+module.exports = function(_deployer) {
+  // Use deployer to state migration tasks.
+};

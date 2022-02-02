18422d9c8bae241c57c46b644f571c894606f8a5
wetzelbe <benedict.f.wetzel@gmx.de>
2022-02-02 14:40:26 +0100

    Update Tests

--- test/bpool_test.js
+++ test/bpool_test.js
@@ -1,24 +1,26 @@
 const BFactory = artifacts.require("BFactory");
 const BPool = artifacts.require("BPool");
+const TToken = artifacts.require("TToken")
 /*
  * uncomment accounts to access the test accounts made available by the
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
 contract("BFactory", function (/* accounts */) {
-  it("is deployed", async function () {
-    let contr = undefined;
-    try {
-      contr = await BFactory.deployed();
-    } catch (error) {}
-    return assert.notEqual(contr, undefined);
+  let factory;
+  let pool;
+  let tx;
+  let poolAddress;
+  before(async () => {
+    factory = await BFactory.deployed();
+    tx = await factory.newBPool();
+    poolAddress = tx.logs[0].args.pool;
+    pool = await BPool.at(poolAddress);
   });
-  it("creates new pools", async function () {
-    let factory = await BFactory.deployed();
-    let tx = await factory.newBPool();
-    let poolAddress = tx.logs[0].args.pool; 
-    let pool = await BPool.at(poolAddress);
-    assert.equal(await pool.symbol.call(), "CPT");
-
-  });
+  it("creates new pool", async () => {
+      assert.equal(await pool.symbol.call(), "CPT");
+    });
+  it("isBPool returns true on new pool", async () => {
+      assert.isTrue(await factory.isBPool.call(poolAddress));
+    })
 });
--- test/tourtoken_test.js
+++ test/tourtoken_test.js
@@ -6,28 +6,27 @@ const TourToken = artifacts.require("TourTokenTemplate");
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
 contract("TTFactory", function (/* accounts */) {
-  it("is deployed", async function () {
+  let factory;
+  let creationEvent;
+  let newTourTokenAddress;
+  let newTourToken;
+  before(async () => {
     factory = await TTFactory.deployed();
-    assert.notEqual(factory, undefined)
+    creationEvent = await factory.createToken("1234", "TOUR1234", "TOUR1234", 10);
+    newTourTokenAddress = creationEvent.logs[0].args['0'];
+    newTourToken = await TourToken.at(newTourTokenAddress);
   })
-  it("adds one to Count when creating a Token", async function () {
-    factory = await TTFactory.deployed();
-    let countbefore = await factory.getCurrentTokenCount.call();
-    await factory.createToken("1234", "TOUR1234", "TOUR1234", 10)
-    let countafter = await factory.getCurrentTokenCount.call();
-    return assert.equal(countbefore.toNumber() + 1, countafter);
-  });
-  it("Returns right number of Events when creating new TourToken", async function () {
-    factory = await TTFactory.deployed();
-    let newtoken = await factory.createToken("1234", "TOUR1234", "TOUR1234", 10)
-    return assert.equal(newtoken.logs.length, 3);
-  });
-  it("Returns right name after creating new TourToken", async function () {
-    factory = await TTFactory.deployed();
-    let creationEvent = await factory.createToken("1234", "TOUR1234", "TOUR1234", 10)
-    let newTourTokenAddress = creationEvent.logs[0].args['0'];
-    let newTourToken = await TourToken.at(newTourTokenAddress);
-    let name = await newTourToken.name.call();
-    return assert.equal(name, "TOUR1234");
-  });
+  it("adds one to Count when creating a Token", async () => {
+      let countbefore = await factory.getCurrentTokenCount.call();
+      await factory.createToken("1234", "TOUR1234", "TOUR1234", 10);
+      let countafter = await factory.getCurrentTokenCount.call();
+      return assert.equal(countbefore.toNumber() + 1, countafter);
+    });
+  it("Returns right number of Events when creating new TourToken", async () => {
+      return assert.equal(creationEvent.logs.length, 3);
+    });
+  it("Returns right name after creating new TourToken", async () => {
+      let name = await newTourToken.name.call();
+      return assert.equal(name, "TOUR1234");
+    });
 });
